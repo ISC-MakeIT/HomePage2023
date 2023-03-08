42bc4be42288768c465d79eff23a0bf569e6c329
YahiroRyo <honakafn@gmail.com>
2023-03-08 14:18:38 +0900

    活動実績一覧取得API実装

---
+++ api/app/Http/Resources/Work/User/WorksResource.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Resources\Work\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WorksResource extends JsonResource {
+    public function toArray(Request $request): array {
+        return [
+            'workId'    => $this->work_id,
+            'title'     => $this->title,
+            'contents'  => $this->contents,
+            'createdAt' => $this->created_at,
+        ];
+    }
+}
