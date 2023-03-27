b116d98e569f9376d06e28c9d8e8f196999620a0
kentyisapen <kenkenpoco@gmail.com>
2023-03-27 14:24:33 +0900

    add: WorkComponent

--- client/src/components/pages/index.tsx
+++ client/src/components/pages/index.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { Helmet } from 'react-helmet';
 import { Header } from '../organisms/Header';
 import { Top } from '../organisms/Top';
+import { Work } from '../organisms/user/presentationalComponents/Work/Index';
 
 export const Home = () => {
   return (
@@ -17,6 +18,7 @@ export const Home = () => {
       >
         <Header />
         <Top />
+        <Work />
       </div>
     </div>
   );
