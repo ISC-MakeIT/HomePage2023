a3b9466ea38c2604b6a0cfb92d44e5bb3697dbfb
YahiroRyo <honakafn@gmail.com>
2023-04-05 20:08:49 +0900

    ファイルディレクトリ修正

--- scripts_for_deploy/after_install.sh
+++ scripts_for_deploy/after_install.sh
@@ -4,8 +4,8 @@ main() {
     sudo find /var/www/html/api -type f -exec chmod 664 {} \;
     sudo find /var/www/html/api -type d -exec chmod 774 {} \;
 
-    sudo chmod -R 775 /var/www/app/storage
-    sudo chmod -R 775 /var/www/app/bootstrap/cache
+    sudo chmod -R 775 /var/www/html/api/storage
+    sudo chmod -R 775 /var/www/html/api/bootstrap/cache
 }
 
 main
