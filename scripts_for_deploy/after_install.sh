d3923e19527a39d6574f0af522b8375273ccc502
YahiroRyo <honakafn@gmail.com>
2023-04-05 15:03:58 +0900

    あーなるほどなあ

--- scripts_for_deploy/after_install.sh
+++ scripts_for_deploy/after_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 main() {
-    sudo find ./api -type d -exec chmod 755 {} +
-    sudo find ./api -type f -exec chmod 644 {} +
-    sudo chmod -R 777 ./api/storage
+    sudo chmod -R 777 ./api
 
     composer install --working-dir=./api
 }
