fbb184745d19873d1b560c6e80590cc2465d4459
YahiroRyo <honakafn@gmail.com>
2023-04-05 14:07:55 +0900

    わあ

--- scripts_for_deploy/after_install.sh
+++ scripts_for_deploy/after_install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 main() {
-    php /var/www/html/api/artisan clear-compiled
-    php /var/www/html/api/artisan optimize
-    php /var/www/html/api/artisan view:clear
-    php /var/www/html/api/artisan cache:clear
-
     sudo find /var/www/html/api -type d -exec chmod 755 {} +
     sudo find /var/www/html/api -type f -exec chmod 644 {} +
-    chmod -R 777 /var/www/html/api/storage
+    sudo chmod -R 777 /var/www/html/api/storage
 
     composer install --working-dir=/var/www/html/api
 }
