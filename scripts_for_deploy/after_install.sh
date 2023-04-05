9f6f5c495cbeb5932d2a15e39c87693cab0a3ffb
YahiroRyo <honakafn@gmail.com>
2023-04-05 19:47:36 +0900

    ﾏｼﾞﾑﾘ

--- scripts_for_deploy/after_install.sh
+++ scripts_for_deploy/after_install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 main() {
-    composer install --working-dir=./api
+    find /var/www/html/api -type f -exec chmod 664 {} \;
+    find /var/www/html/api -type d -exec chmod 774 {} \;
+
+    chmod -R 775 /var/www/app/storage
+    chmod -R 775 /var/www/app/bootstrap/cache
+
+    # composer
+    cd /var/www/html/api &&
+    composer install
 }
 
 main
