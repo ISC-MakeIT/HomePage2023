77c511f7e86a154b47f7f51440cc3ec6c764d77f
kentyisapen <kenkenpoco@gmail.com>
2023-04-07 05:17:20 +0900

    add: route設定

---
+++ client/src/components/pages/user/notifications/index.tsx
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+export const Notifications = () => {
+  return (
+    <div
+      css={css`
+        background-color: black;
+      `}
+    >
+      aaaaa
+    </div>
+  );
+};
--- client/src/routes/userRoutes/index.tsx
+++ client/src/routes/userRoutes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../../components/pages';
 import { USER_ROUTE_PATH_MAP } from '../routePath';
+import { Notifications } from 'src/components/pages/user/notifications';
 
 export const UserRoutes = () => {
   return (
     <Routes>
       <Route path={USER_ROUTE_PATH_MAP.TOP} element={<Home />} />
+      <Route path={USER_ROUTE_PATH_MAP.NOTIFICATIONS} element={<Notifications />} />
     </Routes>
   );
 };
