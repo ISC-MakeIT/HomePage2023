5fd940ab22824ff885fa1af82d19a74c7d1aba32
yappi <76673979+YahiroRyo@users.noreply.github.com>
2023-04-18 09:34:17 +0900

    Revert "エラー修正"

--- client/src/components/organisms/admin/containerComponents/CreateWorkModalContainer/index.tsx
+++ client/src/components/organisms/admin/containerComponents/CreateWorkModalContainer/index.tsx
@@ -38,7 +38,7 @@ export const CreateWorkModalContainer = () => {
       reset();
       setIsActive(false);
 
-      router.push(ADMIN_ROUTE_FULL_PATH_MAP.WORKS);
+      router(ADMIN_ROUTE_FULL_PATH_MAP.WORKS);
     } catch (e) {
       if (axios.isAxiosError(e)) {
         const status = e.response!.status;
